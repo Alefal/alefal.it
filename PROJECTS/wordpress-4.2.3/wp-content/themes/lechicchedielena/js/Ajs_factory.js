008251fb1a63eb9b06eeb04798e68d7293f8f078
Alefal <alefalwebmaster@gmail.com>
2015-08-16 14:00:21 +0200

    content

--- PROJECTS/wordpress-4.2.3/wp-content/themes/lechicchedielena/js/Ajs_factory.js
+++ PROJECTS/wordpress-4.2.3/wp-content/themes/lechicchedielena/js/Ajs_factory.js
@@ -4,11 +4,13 @@ angular.module('lechicchedielena')
 
         var ajaxCallServices = {};
         //var urlBase = 'http://localhost/amalficoastapps.it/lechicchedielena.it/json/';
-        var urlBase = 'wp-content/themes/lechicchedielena/json/';
+        //var urlBase = 'wp-content/themes/lechicchedielena/json/';
+        var urlBase = '?json=get_tag_posts&tag_slug=products';
 
         /***** getCollections ****/
         ajaxCallServices.getCollections = function () {
-            return $http.get(urlBase+ 'collections.json');
+            //return $http.get(urlBase+ 'collections.json');
+            return $http.get(urlBase);
         };
 
         return ajaxCallServices;
