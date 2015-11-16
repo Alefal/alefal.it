c7b46ca07919e996f180238a312a45b48cceacff
Alefal <alefalwebmaster@gmail.com>
2015-11-17 00:36:26 +0100

    Mertodi e JSON

--- MOBILE/ionic/cds/assets/www/js/controllers.js
+++ MOBILE/ionic/cds/assets/www/js/controllers.js
@@ -6,30 +6,124 @@ angular.module('starter.controllers', [])
   });
 
 
-  /***** TEST REST CALL 
-  ajaxCallServices.getReservation('')
+    /***** TEST REST CALL *****/
+    ///////////////////////// GIFT
+    
+    ajaxCallServices.getAllGift('2010-12-32','A02ado')
       .success(function (data) {
-        console.log('data --->'+JSON.stringify(data));
+        console.log('getAllGift --->'+JSON.stringify(data));
+
+        $ionicLoading.hide();
+      }).error(function (error) {
+        $scope.status = 'Unable to load customer data' + error;
+      });
+    /*
+    ajaxCallServices.updateGift('1','22')
+      .success(function (data) {
+        console.log('updateGift --->'+JSON.stringify(data));
+
+        $ionicLoading.hide();
+      }).error(function (error) {
+        $scope.status = 'Unable to load customer data' + error;
+      });
+    
+    ajaxCallServices.addGift('2010-12-32','A01moder','2','PEPSI','fsfsad')
+      .success(function (data) {
+        console.log('addGift --->'+JSON.stringify(data));
+
+        $ionicLoading.hide();
+      }).error(function (error) {
+        $scope.status = 'Unable to load customer data' + error;
+      });
+    
+    ajaxCallServices.deleteGift('2')
+      .success(function (data) {
+        console.log('deleteGift --->'+JSON.stringify(data));
+
+        $ionicLoading.hide();
+      }).error(function (error) {
+        $scope.status = 'Unable to load customer data' + error;
+      });
+    */
+    ///////////////////////// RESERVATION
+    /*
+    ajaxCallServices.insertReservation('22','2010-12-32','A03moder','A03','5')
+      .success(function (data) {
+        console.log('insertReservation --->'+JSON.stringify(data));
 
         $ionicLoading.hide();
       }).error(function (error) {
         $scope.status = 'Unable to load customer data' + error;
       });
 
-
-
-  ajaxCallServices.login('ivoru')
+    ajaxCallServices.updateReservation('1','22','2010-12-32','A01moder','A01','5')
       .success(function (data) {
-        console.log('data --->'+JSON.stringify(data));
+        console.log('updateReservation --->'+JSON.stringify(data));
 
         $ionicLoading.hide();
       }).error(function (error) {
         $scope.status = 'Unable to load customer data' + error;
       });
 
-    ajaxCallServices.getMenu('beverages')
+    ajaxCallServices.deleteReservation('1','22','2010-12-32','A01moder','A01','5')
       .success(function (data) {
-        console.log('data --->'+JSON.stringify(data));
+        console.log('deleteReservation --->'+JSON.stringify(data));
+
+        $ionicLoading.hide();
+      }).error(function (error) {
+        $scope.status = 'Unable to load customer data' + error;
+      });
+
+    ajaxCallServices.getAllReservation('2010-12-32','ivoru')
+      .success(function (data) {
+        console.log('getAllReservation --->'+JSON.stringify(data));
+
+        $ionicLoading.hide();
+      }).error(function (error) {
+        $scope.status = 'Unable to load customer data' + error;
+      });
+
+    ajaxCallServices.getReservationForWaiters('2010-12-32','ivoru')
+      .success(function (data) {
+        console.log('getReservationForWaiters --->'+JSON.stringify(data));
+
+        $ionicLoading.hide();
+      }).error(function (error) {
+        $scope.status = 'Unable to load customer data' + error;
+      });
+
+    ajaxCallServices.getReservation('2010-12-32','A01moder')
+      .success(function (data) {
+        console.log('getReservation --->'+JSON.stringify(data));
+
+        $ionicLoading.hide();
+      }).error(function (error) {
+        $scope.status = 'Unable to load customer data' + error;
+      });
+    
+    ajaxCallServices.changeReservationTable('2010-12-32','A01moder','A01')
+      .success(function (data) {
+        console.log('changeReservationTable --->'+JSON.stringify(data));
+
+        $ionicLoading.hide();
+      }).error(function (error) {
+        $scope.status = 'Unable to load customer data' + error;
+      });
+    */
+    ///////////////////////// OTHER
+    /*
+    ajaxCallServices.login('ivoru')
+      .success(function (data) {
+        console.log('login --->'+JSON.stringify(data));
+
+        $ionicLoading.hide();
+      }).error(function (error) {
+        $scope.status = 'Unable to load customer data' + error;
+      });
+
+    ajaxCallServices.getMenu('bottle')
+      .success(function (data) {
+        console.log('getMenu --->'+JSON.stringify(data));
 
         $ionicLoading.hide();
       }).error(function (error) {
@@ -38,13 +132,14 @@ angular.module('starter.controllers', [])
 
     ajaxCallServices.getRoles()
       .success(function (data) {
-        console.log('data --->'+JSON.stringify(data));
+        console.log('getRoles --->'+JSON.stringify(data));
 
         $ionicLoading.hide();
       }).error(function (error) {
         $scope.status = 'Unable to load customer data' + error;
       });
-    *****/
+    */
+    
   $ionicLoading.hide();
 })
 .controller('LoginCtrl', function($scope,$rootScope,$ionicLoading,ajaxCallServices,$state/*,$cordovaNetwork*/) {
--- MOBILE/ionic/cds/assets/www/js/services.js
+++ MOBILE/ionic/cds/assets/www/js/services.js
@@ -2,43 +2,43 @@
 Tutti I servizi hanno "security policy provided by Spring". Di conseguenza sarà necessario inviare ad ogni chiamata le credenziali di autenticazione.
 
 RESERVATION
-- getReservationForWaiters(Date,User)  			  get all tables assigned to a waiter
-- getReservation(Reservation)                 get reservation details
-- getAllReservation(Date)                     get all resevation of a date
-- updateReservation(Reservatio)             	update a reservation info
-- InsertReservation(Reservation)             	insert reservation
-- deleteReservation(Reservation)            	delete a reservation
-- changeReservationTable(Reservation, Table)  change reservation table
+OK -> getReservationForWaiters()  			get all tables assigned to a waiter
+OK -> getReservation()                  get reservation details
+OK -> getAllReservation()               get all resevation of a date
+OK -> updateReservation()             	update a reservation info
+OK -> insertReservation()             	insert reservation
+OK -> deleteReservation()            	  delete a reservation
+OK -> changeReservationTable()          change reservation table
 
 GIFT
-- getAllGift(Reservation)                     get all gift of a reservation
-- deleteGift(Reservation)                     delete a gift given to a reservation
-- addGift(Gift)                               add a gift to a guest
-- updateGift(Gift)                         		update gift 
-- setOrderAsGift(Order)                 		  set order as a gift
+OK -> getAllGift()                      get all gift of a reservation
+OK -> deleteGift()                      delete a gift given to a reservation
+OK -> addGift()                         add a gift to a guest
+OK -> updateGift()                      update gift 
+- setOrderAsGift()                 		  set order as a gift
 
 DISCOUNT
-- deleteDiscount(Reservation)               	delete a gift given to a reservation 
-- setDiscount(Reservation, Order)         		set a discount for a guest
-- getAllDiscount(Reservation)                	get the discount of a reservation
+- deleteDiscount()               	      delete a gift given to a reservation 
+- setDiscount()         		            set a discount for a guest
+- getAllDiscount()                	    get the discount of a reservation
 
 ORDER
-- getOrders(Reservation)                 		  getAllOrders of a reservation
-- getAllOrders(Date)                         	getAllOrders of a day
-- getOrdinationType()                       	get all the available type of the staff to order
-- getOrderForWaiter(User)               		  get all order from some table assigned to a given 
-- updateOrder(Order)                       		update the state of an order 
-- makeOrder(Order)                         		send the order
+- getOrders()                 		      getAllOrders of a reservation
+- getAllOrders()                        getAllOrders of a day
+- getOrdinationType()                   get all the available type of the staff to order
+- getOrderForWaiter()               		get all order from some table assigned to a given 
+- updateOrder()                       	update the state of an order 
+- makeOrder()                         	send the order
 
 OTHER
-- getRequest (Request request)        			  get pending tasks for a client
-- getMenu()                                   get the menu of the club
-OK -> login(User)                                 login service
-- getRoles()                                  ruoli disponibili
-- getAvailableTable (Date)               		  restituisce la lista di tavoli liberi per una data
-- getMap(Date)                                restituisce il percorso dell imagine della mappa
-- getBillPDF(ReservationTO)             		  genera un pdf contenente il totale del conto ed il  
-- callWaiter(Table)                           chiama il cameriere.
+- getRequest ()        			            get pending tasks for a client
+OK -> getMenu()                         get the menu of the club
+OK -> login()                           login service
+OK -> getRoles()                        ruoli disponibili
+- getAvailableTable()               		restituisce la lista di tavoli liberi per una data
+- getMap()                              restituisce il percorso dell imagine della mappa
+- getBillPDF()             		          genera un pdf contenente il totale del conto ed il  
+- callWaiter()                          chiama il cameriere.
 *****/
 
 angular.module('starter.factory', [])
@@ -46,45 +46,182 @@ angular.module('starter.factory', [])
 .factory('ajaxCallServices', function($http) {
 
   var ajaxCallServices = {};
-  //var urlBase = 'http://champagneriadellasala-disconnection.rhcloud.com/services/';
-  var urlBase = 'http://localhost/services/';
+  var urlBase = 'http://champagneriadellasala-disconnection.rhcloud.com/services/';
+  //var urlBase = 'http://localhost/services/';
 
   /***** getRequest (Request request)  *****/
 
 
+  //var usernameService = 'moder';
+  //var passwordService = '111111';
 
+  var usernameService = 'adm';
+  var passwordService = '222222';
 
+  ///////////////////////// GIFT
+  /***** OK: getAllGift() *****/
+  ajaxCallServices.getAllGift = function (date_reservation,code) {
+    var parameter = JSON.stringify({"date_reservation":date_reservation,"code":code});
 
-  /***** KO: getReservation(Reservation) *****/
-  ajaxCallServices.getReservation = function (reservation) {
-    var parameter = JSON.stringify({"id_reservation":null,"people_num":null,"date_reservation":null,"code":"A01moder","note":null,"code_table":"","user":null});
+    return $http({
+      url:urlBase+'nightService/getAllGift/',
+      method:'POST',
+      data: parameter,
+      headers: { 
+        'Authorization': 'Basic ' + btoa(usernameService + ':' + passwordService)
+      }
+    });
+  };
+  /***** OK: addGift() *****/
+  ajaxCallServices.addGift = function (date_reservation,code,qty,name,login) {
+    var parameter = JSON.stringify({"reservation_id":{"date_reservation":date_reservation,"code":code},"qty":qty,"stuff_id":{"name":name},"owner_id":{"login":login}});
+
+    return $http({
+      url:urlBase+'nightService/addGift/',
+      method:'POST',
+      data: parameter,
+      headers: { 
+        'Authorization': 'Basic ' + btoa(usernameService + ':' + passwordService)
+      }
+    });
+  };
+  /***** OK: updateGift() *****/
+  ajaxCallServices.updateGift = function (gift_id,qty) {
+    var parameter = JSON.stringify({"gift_id":gift_id,"qty":qty});
+
+    return $http({
+      url:urlBase+'nightService/updateGift/',
+      method:'POST',
+      data: parameter,
+      headers: { 
+        'Authorization': 'Basic ' + btoa(usernameService + ':' + passwordService)
+      }
+    });
+  };
+  /***** OK: deleteGift() *****/
+  ajaxCallServices.deleteGift = function (gift_id) {
+    var parameter = JSON.stringify({"gift_id":gift_id});
+
+    return $http({
+      url:urlBase+'nightService/deleteGift/',
+      method:'POST',
+      data: parameter,
+      headers: { 
+        'Authorization': 'Basic ' + btoa(usernameService + ':' + passwordService)
+      }
+    });
+  };
+
+  ///////////////////////// RESERVATION
+  /***** OK: insertReservation() *****/
+  ajaxCallServices.insertReservation = function (people_num,date_reservation,code,code_table,userId) {
+    var parameter = JSON.stringify({"people_num":people_num,"date_reservation":date_reservation,"code":code,"code_table":{"code":code_table},"user":{"id":userId}});
+
+    return $http({
+      url:urlBase+'nightService/insertReservation/',
+      method:'POST',
+      data: parameter,
+      headers: { 
+        'Authorization': 'Basic ' + btoa(usernameService + ':' + passwordService)
+      }
+    });
+  };
+  /***** OK: updateReservation() *****/
+  ajaxCallServices.updateReservation = function (id_reservation,people_num,date_reservation,code,code_table,userId) {
+    var parameter = JSON.stringify({"id_reservation":id_reservation,"people_num":people_num,"date_reservation":date_reservation,"code":code,"code_table":{"code":code_table},"user":{"id":userId}});
+
+    return $http({
+      url:urlBase+'nightService/updateReservation/',
+      method:'POST',
+      data: parameter,
+      headers: { 
+        'Authorization': 'Basic ' + btoa(usernameService + ':' + passwordService)
+      }
+    });
+  };
+  /***** OK: deleteReservation() *****/
+  ajaxCallServices.deleteReservation = function (id_reservation,people_num,date_reservation,code,code_table,userId) {
+    var parameter = JSON.stringify({"id_reservation":id_reservation,"people_num":people_num,"date_reservation":date_reservation,"code":code,"code_table":{"code":code_table},"user":{"id":userId}});
+
+    return $http({
+      url:urlBase+'nightService/deleteReservation/',
+      method:'POST',
+      data: parameter,
+      headers: { 
+        'Authorization': 'Basic ' + btoa(usernameService + ':' + passwordService)
+      }
+    });
+  };
+  /***** OK: getAllReservation() *****/
+  ajaxCallServices.getAllReservation = function (date_reservation) {
+    var parameter = JSON.stringify({"date_reservation":date_reservation});
+
+    return $http({
+      url:urlBase+'nightService/getAllReservation/',
+      method:'POST',
+      data: parameter,
+      headers: { 
+        'Authorization': 'Basic ' + btoa(usernameService + ':' + passwordService)
+      }
+    });
+  };
+  /***** OK: getReservationForWaiters() *****/
+  ajaxCallServices.getReservationForWaiters = function (date_reservation,user) {
+    var parameter = JSON.stringify({"date":date_reservation,"user":{"login":user}});
+
+    return $http({
+      url:urlBase+'nightService/getReservationForWaiters/',
+      method:'POST',
+      data: parameter,
+      headers: { 
+        'Authorization': 'Basic ' + btoa(usernameService + ':' + passwordService)
+      }
+    });
+  };
+  /***** OK: getReservation() *****/
+  ajaxCallServices.getReservation = function (date_reservation,code) {
+    var parameter = JSON.stringify({"date_reservation":date_reservation,"code":code});
 
     return $http({
       url:urlBase+'nightService/getReservation/',
       method:'POST',
       data: parameter,
       headers: { 
-        'Authorization': 'Basic bW9kZXI6MTExMTEx' 
+        'Authorization': 'Basic ' + btoa(usernameService + ':' + passwordService)
+      }
+    });
+  };
+  /***** OK: changeReservationTable() *****/
+  ajaxCallServices.changeReservationTable = function (date_reservation,code,code_table) {
+    var parameter = JSON.stringify({"date_reservation":date_reservation,"code":code,"code_table":{"code":code_table}});
+
+    return $http({
+      url:urlBase+'nightService/changeReservationTable/',
+      method:'POST',
+      data: parameter,
+      headers: { 
+        'Authorization': 'Basic ' + btoa(usernameService + ':' + passwordService)
       }
     });
   };
 
+  ///////////////////////// OTHER
   /***** OK: getMenu() *****/
   ajaxCallServices.getMenu = function (category) {
     //{"category":null}
-    //VALORI POSSIBILI: bottles, beverages
-    var parameter = JSON.stringify({"category":null});
+    //VALORI POSSIBILI: bottle, beverages
+    var parameter = JSON.stringify({"category":category});
 
     return $http({
       url:urlBase+'nightService/getMenu/',
       method:'POST',
       data: parameter,
       headers: { 
-        'Authorization': 'Basic bW9kZXI6MTExMTEx' 
+        'Authorization': 'Basic ' + btoa(usernameService + ':' + passwordService)
       }
     });
   };
-  /***** OK: login(User) *****/
+  /***** OK: login() *****/
   ajaxCallServices.login = function (username) {
     var parameter = JSON.stringify({login:username});
 
@@ -93,18 +230,17 @@ angular.module('starter.factory', [])
       method:'POST',
       data: parameter,
       headers: { 
-        'Authorization': 'Basic bW9kZXI6MTExMTEx'
+        'Authorization': 'Basic ' + btoa(usernameService + ':' + passwordService)
       }
     });
   };
-
   /**** OK: getRoles() *****/
   ajaxCallServices.getRoles = function () {
     return $http({
       url:urlBase+'baseService/getRoles/',
       method:'GET',
       headers: { 
-        'Authorization': 'Basic bW9kZXI6MTExMTEx'
+        'Authorization': 'Basic ' + btoa(usernameService + ':' + passwordService)
       }
     });
   };
