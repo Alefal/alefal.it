ddec0cbff59aa7c8d7778cb682c29ccf9d062bdc
Alefal <alefalwebmaster@gmail.com>
2015-11-27 16:06:21 +0100

    WP plugin
    sostituisce alefal_notificationGCM, alefal_sendEmail e include service
    per UPDATE APP

---
+++ PLUGINS/wordpress/alefal_servicesUtility/services/notification/removeDevice.php
@@ -0,0 +1,10 @@
+<?php
+require_once('../../../../../wp-config.php'); 
+require_once('../functions.php');
+
+global $wpdb;
+$table_name = 'alfl_register_device';
+
+if(isset($_POST['regId'])) {
+	$wpdb->delete( $table_name, array( 'registerId' => $_POST['regId'] ) );
+}
