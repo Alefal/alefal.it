d1886e27f369e492afd85e50a19071c5431d073a
Alefal <alefalwebmaster@gmail.com>
2016-03-08 17:30:59 +0100

    Alefal.it
    Bootstrap theme

--- PROJECTS/amalfitourscarservice/wp-content/themes/amalfitourscarservice/index.php
+++ PROJECTS/amalfitourscarservice/wp-content/themes/amalfitourscarservice/index.php
@@ -11,23 +11,8 @@
 	                <i class="fa fa-angle-down fa-2x"></i>
 	            </span>
 			</div>
-	        </a>
+	    </a>
 	</section>
-INDEX
-	
-		<?php if ( have_posts() ) : ?>
-
-			<?php /* The loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
-
-			<?php twentythirteen_paging_nav(); ?>
-
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-		<?php endif; ?>
-
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
