78ec6de2004c345f9c605587b0f606e0dca77385
Alefal <alefalwebmaster@gmail.com>
2016-05-09 18:04:57 +0200

    WP WooCommerce Plugin and Stock Plugin

--- PROJECTS/stockmagazine/application/config/config.php
+++ PROJECTS/stockmagazine/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://10.80.18.107/alefal.it/PROJECTS/stockmagazine/';
+$config['base_url']	= 'http://10.80.18.147/alefal.it/PROJECTS/stockmagazine/';
 //$config['base_url']	= 'http://www.amalficoastapp.it/comingsoon/stockmagazine/';
 
 /*
