f5756e8df8d19602d7439c8c45a84f5b16f9ad9d
Alefal <alefalwebmaster@gmail.com>
2016-09-02 13:13:32 +0200

    Facebook Login

---
+++ MOBILE/ionic/VERS.2/loginFacebook/app/pages/home/home.ts
@@ -0,0 +1,53 @@
+import {Component} from '@angular/core';
+import {NavController, Platform} from 'ionic-angular';
+import { Facebook } from 'ionic-native';
+
+import {FbProvider} from '../../providers/fb-provider/fb-provider';
+
+@Component({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+
+  posts: any;
+
+  constructor(
+    private navCtrl: NavController,
+    platform: Platform,
+    fbProvider: FbProvider
+  ) {
+    /** Login */
+    fbProvider.login().then((responseLogin) => {
+      console.log('responseLogin: ', JSON.stringify(responseLogin));
+
+      /** Me */
+      fbProvider.me().then((responseMe) => {
+        console.log('me: ', JSON.stringify(responseMe));
+        console.log('Good to see you, ' + responseMe.name + '.');
+      }, (err) => {
+        console.error('Unable to execute me: ', err);
+      });
+      
+    }, (err) => {
+      console.error('Unable to execute login: ', err);
+    });
+    
+    /** Posts Page */
+    fbProvider.postsPage().then((responsePostsPage) => {
+      console.log('me: ', JSON.stringify(responsePostsPage));
+      this.posts = responsePostsPage.data;
+    }, (err) => {
+      console.error('Unable to execute postsPage: ', err);
+    });
+
+
+
+    /** AppInvite
+    fbProvider.appInvite().then((responseAppInvite) => {
+      console.log('appInvite: ', JSON.stringify(responseAppInvite));
+    }, (err) => {
+      console.error('Unable to execute appInvite: ', err);
+    });
+    */
+  }
+}
