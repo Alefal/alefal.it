2dfa5a4de2561dd6e22ee8b34c4cd34627a8de66
Alefal <alefalwebmaster@gmail.com>
2016-11-01 09:10:22 +0100

    Clear

--- BLOG/demo/angular2/quickstart/app/main.ts
+++
@@ -1,8 +0,0 @@
-import {bootstrap}    	from '@angular/platform-browser-dynamic';
-import {AppComponent} 	from './app.component';
-import {User} 			from './generic';	//Importo la classe definita con la parola chiave "export"
-
-bootstrap(AppComponent);
-
-var user = new User('Ale', 'Fal');
-alert(user.hello());
