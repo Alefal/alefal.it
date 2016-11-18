5fcef781deaf024fe8632329abc441659a96ece0
Alefal <alefalwebmaster@gmail.com>
2016-11-18 16:50:42 +0100

    WHE: client e prdunct non vengono salvati

--- PROJECTS/ece/wp-content/plugins/alefal_woocommerce/services/ece_order_save.php
+++ PROJECTS/ece/wp-content/plugins/alefal_woocommerce/services/ece_order_save.php
@@ -6,13 +6,23 @@ $eceResultArray     = array();
 $eceOutputArray     = array();
 $eceFinalArray      = array();
 
-$order = json_decode($_GET['order'],true);
+//$order = json_decode($_GET['order'],true);
+//$order = json_decode('{"id":0,"status":"pending","total":149.97,"total_tax":0,"total_line_items_quantity":6,"line_items":[{"id":110,"title":"Prodotto 2","price":"20","description":"<p>Prodotto 2: quantità iniziale 50</p>\n","stock":90,"images":[{"id":0,"created_at":"2016-11-18T15:43:53Z","updated_at":"2016-11-18T15:43:53Z","src":"http://localhost/alefal.it/PROJECTS/ece/wp-content/plugins/woocommerce/assets/images/placeholder.png","title":"Segnaposto","alt":"Segnaposto","position":0}]},{"id":101,"title":"Prodotto 1","price":"9.99","description":"<p>Prodotto 1: quantità iniziale 100</p>\n","stock":135,"images":[{"id":0,"created_at":"2016-11-18T15:43:53Z","updated_at":"2016-11-18T15:43:53Z","src":"http://localhost/alefal.it/PROJECTS/ece/wp-content/plugins/woocommerce/assets/images/placeholder.png","title":"Segnaposto","alt":"Segnaposto","position":0}]}],"customer":[{"display_name":"cliente","user_nicename":"cliente","user_email":"alefal1@hotmail.it"}],"note":"Ordine salvato"}',true);
+
+$order = json_decode('{"id":0,"status":"pending","total":149.97,"total_tax":0,"total_line_items_quantity":6,"line_items":"","stock":135,"images":"","customer":"","note":"Ordine salvato"}',true);
+
 $id = $order['id'];
 //print_r($order);
 //die();
 
 $orderArray = array(
-    'status'    => 'pending'
+    'status'                    => $order['status'],
+    'total'                     => $order['total'],
+    'total_tax'                 => $order['total_tax'],
+    'total_line_items_quantity' => $order['total_line_items_quantity'],
+    'line_items'                => $order['line_items'],
+    'customer'                  => $order['customer'],
+    'note'                      => $order['note'],
 );
 
 try {
