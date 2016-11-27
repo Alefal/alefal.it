cc973cff0aee4c50eae925d2ecbd4c531dfcd057
Alefal <alefalwebmaster@gmail.com>
2016-11-28 00:59:25 +0100

    LE: torneo dei tifosi 2016

--- PROJECTS/leagueengine/wp-content/plugins/alefal_leagueengine/functions.php
+++ PROJECTS/leagueengine/wp-content/plugins/alefal_leagueengine/functions.php
@@ -11,7 +11,7 @@ $season_id = 96;
 //Locale: Tournament leagueknockout
 //$tournament_id = 5;
 //Remote: Tournament test 2x3
-$tournament_id = 76;
+//$tournament_id = 59;
 
 function le_leagueengine_table_gp($league,$season,$team) {
 	global $wpdb;
