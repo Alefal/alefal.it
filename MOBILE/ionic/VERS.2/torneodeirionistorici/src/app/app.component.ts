a6995a493a03715c51f83d29954c24a148c90831
Alefal <alefalwebmaster@gmail.com>
2016-11-28 16:53:19 +0100

    LE: push notification

--- MOBILE/ionic/VERS.2/torneodeirionistorici/src/app/app.component.ts
+++ MOBILE/ionic/VERS.2/torneodeirionistorici/src/app/app.component.ts
@@ -1,3 +1,5 @@
+//declare var push: any;
+
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Push } from 'ionic-native';
@@ -12,10 +14,64 @@ import { HomePage } from '../pages/home/home';
 export class TorneoRioniStorici {
   rootPage = HomePage;
 
-  /*
-  private httpService: HttpService;
+  constructor(platform: Platform, private httpService: HttpService) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+
+      //this.pushNotifications();
+
+      //cordova plugin add phonegap-plugin-push --variable SENDER_ID="274440871330"
+      console.log('constructor');
+
+      var push = Push.init({
+        android: {
+          vibrate: true,
+          sound: true,
+          senderID: "274440871330"
+        },
+        ios: {
+          alert: "true",
+          badge: true,
+          sound: 'false'
+        },
+        windows: {}
+      });
+
+      console.log('push: '+push);
+
+      push.on('registration', (data) => {
+        console.log(data.registrationId);
+        alert(data.registrationId.toString());
+        
+        //Error in Success callbackId: PushNotification272037515 : TypeError: Cannot read property 'getCallHttp' of undefined
+        this.httpService
+          .getCallHttp('registrationDevice',data.registrationId,'','','','')
+          .then(res => {
+            if(res[0].response[0].result == 'OK') {
+              console.log('data: '+data);
+            }
+          })
+          .catch(error => {
+            console.log('ERROR: ' + error);
+          });
+        
+      });
+      push.on('notification', (data) => {
+        console.log(data);
+        alert("Hi, Am a push notification");
+      });
+      push.on('error', (e) => {
+        console.log(e.message);
+      });
+    });
+  }
+
+/*
   pushNotifications(): void {
-    var push = Push.init({
+
+    push = Push.init({
       android: {
         vibrate: true,
         sound: true,
@@ -54,56 +110,5 @@ export class TorneoRioniStorici {
       console.log(e.message);
     });
   }
-  */
-
-  constructor(platform: Platform, private httpService: HttpService) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-
-      //this.pushNotifications();
-
-/***
-      var push = Push.init({
-        android: {
-          vibrate: true,
-          sound: true,
-          senderID: "274440871330"
-        },
-        ios: {
-          alert: "true",
-          badge: true,
-          sound: 'false'
-        },
-        windows: {}
-      });
-
-      push.on('registration', (data) => {
-        console.log(data.registrationId);
-        //alert(data.registrationId.toString());
-        
-        //Error in Success callbackId: PushNotification272037515 : TypeError: Cannot read property 'getCallHttp' of undefined
-        this.httpService
-          .getCallHttp('registrationDevice',data.registrationId,'','','','')
-          .then(res => {
-            if(res[0].result == 'OK') {
-              console.log('data: '+data);
-            }
-          })
-          .catch(error => {
-            console.log('ERROR: ' + error);
-          });
-        
-      });
-      push.on('notification', (data) => {
-        console.log(data);
-        alert('Hi, Am a push notification: '+JSON.stringify(data));
-      });
-      push.on('error', (e) => {
-        console.log(e.message);
-      });
-***/
-    });
-  }
+*/
 }
