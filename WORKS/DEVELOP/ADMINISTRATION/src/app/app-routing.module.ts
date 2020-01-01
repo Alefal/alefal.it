486c47c76e7a9cb7d482b660edb25b4076f47212
Falcone Alessandro <falcone.alessandro@outlook.com>
2020-01-01 23:46:55 +0100

    Fix

--- WORKS/DEVELOP/ADMINISTRATION/src/app/app-routing.module.ts
+++ WORKS/DEVELOP/ADMINISTRATION/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './shared';
 
 const routes: Routes = [
-    { path: '', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule) },
+    // { path: '', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule) },
+    { path: '', loadChildren: './layout/layout.module#LayoutModule' },
     { path: 'login', loadChildren: './login/login.module#LoginModule' },
     { path: 'access-denied', loadChildren: './access-denied/access-denied.module#AccessDeniedModule' },
     { path: 'not-found', loadChildren: './not-found/not-found.module#NotFoundModule' },
